bb30ca19d4b528763553f9afe297f29972cd86ff
Anne <anr@tradeshift.com>
2016-05-30 13:26:53 +0800

    add filelist to dict

--- doubanmoive/pipelines.py
+++ doubanmoive/pipelines.py
@@ -13,6 +13,8 @@ from doubanmoive.movieReviewItem import MovieReviewItem
 class DoubanmoivePipeline(object):
     def __init__(self):
         self.file = codecs.open('data.dat', mode='wb', encoding='utf-8')
+        self.comment_file_dict = {}
+        self.review_file_dict = {}
 
     def process_item(self, item, spider):
         item_type = type(item)
@@ -20,11 +22,21 @@ class DoubanmoivePipeline(object):
             line = json.dumps(dict(item)) + '\n'
             self.file.write(line.decode("unicode_escape"))
         elif item_type is MovieCommentItem:
-            self.comment_file = codecs.open('./subjects/comments/' + item['movie_id'] + '_comments.dat', mode='ab+', encoding='utf-8')
+            filename = item['movie_id'] + '_comments.dat'
+            if filename not in self.comment_file_dict.keys():
+                self.comment_file = codecs.open('./subjects/comments/' + filename, mode='wb', encoding='utf-8')
+                self.comment_file_dict[filename] = self.comment_file
+            else:
+                self.comment_file = self.comment_file_dict[filename]
             comment = json.dumps(dict(item)) + '\n'
             self.comment_file.write(comment.decode("unicode_escape"))
         elif item_type is MovieReviewItem:
-            self.review_file = codecs.open('./subjects/reviews/' + item['movie_id'] + '_review.dat', mode='ab+', encoding='utf-8')
+            filename = item['movie_id'] + '_review.dat'
+            if filename not in self.review_file_dict.keys():
+                self.review_file = codecs.open('./subjects/comments/' + filename, mode='wb', encoding='utf-8')
+                self.review_file_dict[filename] = self.review_file
+            else:
+                self.review_file = self.review_file_dict[filename]
             line = json.dumps(dict(item)) + '\n'
             self.review_file.write(line.decode("unicode_escape"))
         return item
